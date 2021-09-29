09602ac4f7f9bfbe8882989688f32083d20d7b41
oliverhilstrom <819492@gmail.com>
2021-09-29 14:38:51 -0500

    3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #WRITE YOUR CODE IN THIS FILE
 def close10(x, y):
-    if abs(x) < abs(y):
+    if x - y == 10:
         return abs(x)
     else:
         return abs(y)
-print(close10(50, 9 ))
+print(close10(5, 12 ))
